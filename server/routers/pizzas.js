a81ad38e7ca11d22c0218b11ea706243357d6cc9
Deionne <111102092+DDC76@users.noreply.github.com>
2022-10-10 21:20:01 -0500

    apimongo

--- server/routers/pizzas.js
+++ server/routers/pizzas.js
@@ -11,4 +11,53 @@ router.post("/", (request, response) => {
   });
 });
 
+// Get (Read) all records from the collection
+router.get("/", (request, response) => {
+  Pizza.find({}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+// Get a single record by ID using a query parameter
+router.get("/:id", (request, response) => {
+  Pizza.findById(request.params.id, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+//DELETE
+router.delete("/:id", (request, response) => {
+  Pizza.findByIdAndRemove(request.params.id, {}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+//UPDATE
+router.put("/:id", (request, response) => {
+  const body = request.body;
+  Pizza.findByIdAndUpdate(
+    request.params.id,
+    {
+      $set: {
+        // Take note that the customer is not included, so it can't
+        crust: body.crust,
+        cheese: body.cheese,
+        sauce: body.sauce,
+        toppings: body.toppings
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    },
+    (error, record) => {
+      if (error) return response.status(500).json(error);
+      return response.json(record);
+    }
+  );
+});
+
 module.exports = router;
